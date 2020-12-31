c22841288b14044370631ebb27746ce9f5cb24f9
Szymon Karpiński <hugo@staszic.waw.pl>
2020-12-31 18:36:43 +0100

    Removed tutorial comment from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sinolify", # Replace with your own username
+    name="sinolify",
     version="0.1",
     author="Szymon Karpiński",
     author_email="hugo@informatykanastart.org.pl",
